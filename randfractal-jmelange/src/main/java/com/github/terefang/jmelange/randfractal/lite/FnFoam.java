77323a4642b1bc6caabf98f2817790b67c5d82b4
BuildTools <unconfigured@null.spigotmc.org>
2021-09-23 12:17:57 +0200

    realese early and often

--- randfractal-jmelange/src/main/java/com/github/terefang/jmelange/randfractal/lite/FnFoam.java
+++ randfractal-jmelange/src/main/java/com/github/terefang/jmelange/randfractal/lite/FnFoam.java
@@ -6,24 +6,27 @@ public class FnFoam extends FastNoiseLite
 {
     // ----------------------------------------------------------------------------
 
-    public static final float singleFoam(int interpolation, float foamSharpness, int seed, float x, float y) {
+    public static final float singleFoam(int interpolation, float foamSharpness, boolean _normalizeNoise, int seed, float x, float y) {
         final float p0 = x;
         final float p1 = x * -0.5f + y * 0.8660254037844386f;
         final float p2 = x * -0.5f + y * -0.8660254037844387f;
 
         float xin = p2;
         float yin = p0;
-        final float a = singleValue(interpolation, seed, xin, yin);
+        float a = singleValue(interpolation, seed, xin, yin);
+        if(_normalizeNoise) a = (a * .5f) + .5f;
         seed += 0x9E3779BD;
         seed ^= seed >>> 14;
         xin = p1;
         yin = p2;
-        final float b = singleValue(interpolation, seed, xin + a, yin);
+        float b = singleValue(interpolation, seed, xin + a, yin);
+        if(_normalizeNoise) b = (b * .5f) + .5f;
         seed += 0x9E3779BD;
         seed ^= seed >>> 14;
         xin = p0;
         yin = p1;
-        final float c = singleValue(interpolation, seed, xin + b, yin);
+        float c = singleValue(interpolation, seed, xin + b, yin);
+        if(_normalizeNoise) c = (c * .5f) + .5f;
         final float result = (a + b + c) * F3f;
         final float sharp = foamSharpness * 2.2f;
         final float diff = 0.5f - result;
@@ -31,7 +34,7 @@ public class FnFoam extends FastNoiseLite
         return (((result + sign)) / (Float.MIN_VALUE - sign + (result + sharp * diff) * one) - sign - sign) - 1f;
     }
 
-    public static final float singleFoam(int interpolation, float foamSharpness, int seed, float x, float y, float z){
+    public static final float singleFoam(int interpolation, float foamSharpness, boolean _normalizeNoise, int seed, float x, float y, float z){
         final float p0 = x;
         final float p1 = x * -0.3333333333333333f + y * 0.9428090415820634f;
         final float p2 = x * -0.3333333333333333f + y * -0.4714045207910317f + z * 0.816496580927726f;
@@ -40,26 +43,29 @@ public class FnFoam extends FastNoiseLite
         float xin = p3;
         float yin = p2;
         float zin = p0;
-        final float a = singleValue(interpolation, seed, xin, yin, zin);
+        float a = singleValue(interpolation, seed, xin, yin, zin);
+        if(_normalizeNoise) a = (a * .5f) + .5f;
         seed += 0x9E3779BD;
         seed ^= seed >>> 14;
         xin = p0;
         yin = p1;
         zin = p3;
-        final float b = singleValue(interpolation, seed, xin + a, yin, zin);
+        float b = singleValue(interpolation, seed, xin + a, yin, zin);
+        if(_normalizeNoise) b = (b * .5f) + .5f;
         seed += 0x9E3779BD;
         seed ^= seed >>> 14;
         xin = p1;
         yin = p2;
         zin = p3;
-        final float c = singleValue(interpolation, seed, xin + b, yin, zin);
+        float c = singleValue(interpolation, seed, xin + b, yin, zin);
+        if(_normalizeNoise) c = (c * .5f) + .5f;
         seed += 0x9E3779BD;
         seed ^= seed >>> 14;
         xin = p0;
         yin = p1;
         zin = p2;
-        final float d = singleValue(interpolation, seed, xin + c, yin, zin);
-
+        float d = singleValue(interpolation, seed, xin + c, yin, zin);
+        if(_normalizeNoise) d = (d * .5f) + .5f;
         final float result = (a + b + c + d) * 0.25f;
         final float sharp = foamSharpness * 3.3f;
         final float diff = 0.5f - result;
@@ -68,7 +74,7 @@ public class FnFoam extends FastNoiseLite
 
     }
 
-    public static final float singleFoam(int interpolation, float foamSharpness, int seed, float x, float y, float z, float w)
+    public static final float singleFoam(int interpolation, float foamSharpness, boolean _normalizeNoise, int seed, float x, float y, float z, float w)
     {
         final float p0 = x;
         final float p1 = x * -0.25f + y *  0.9682458365518543f;
@@ -80,36 +86,40 @@ public class FnFoam extends FastNoiseLite
         float yin = p2;
         float zin = p3;
         float win = p4;
-        final float a = singleValue(interpolation, seed, xin, yin, zin, win);
+        float a = singleValue(interpolation, seed, xin, yin, zin, win);
+        if(_normalizeNoise) a = (a * .5f) + .5f;
         seed += 0x9E3779BD;
         seed ^= seed >>> 14;
         xin = p0;
         yin = p2;
         zin = p3;
         win = p4;
-        final float b = singleValue(interpolation, seed, xin + a, yin, zin, win);
+        float b = singleValue(interpolation, seed, xin + a, yin, zin, win);
+        if(_normalizeNoise) b = (b * .5f) + .5f;
         seed += 0x9E3779BD;
         seed ^= seed >>> 14;
         xin = p0;
         yin = p1;
         zin = p3;
         win = p4;
-        final float c = singleValue(interpolation, seed, xin + b, yin, zin, win);
+        float c = singleValue(interpolation, seed, xin + b, yin, zin, win);
+        if(_normalizeNoise) c = (c * .5f) + .5f;
         seed += 0x9E3779BD;
         seed ^= seed >>> 14;
         xin = p0;
         yin = p1;
         zin = p2;
         win = p4;
-        final float d = singleValue(interpolation, seed, xin + c, yin, zin, win);
+        float d = singleValue(interpolation, seed, xin + c, yin, zin, win);
+        if(_normalizeNoise) d = (d * .5f) + .5f;
         seed += 0x9E3779BD;
         seed ^= seed >>> 14;
         xin = p0;
         yin = p1;
         zin = p2;
         win = p3;
-        final float e = singleValue(interpolation, seed, xin + d, yin, zin, win);
-
+        float e = singleValue(interpolation, seed, xin + d, yin, zin, win);
+        if(_normalizeNoise) e = (e * .5f) + .5f;
         final float result = (a + b + c + d + e) * 0.2f;
         final float sharp = foamSharpness * 4.4f;
         final float diff = 0.5f - result;
@@ -117,7 +127,7 @@ public class FnFoam extends FastNoiseLite
         return (((result + sign)) / (Float.MIN_VALUE - sign + (result + sharp * diff) * one) - sign - sign) - 1f;
     }
 
-    public static final float singleFoam(int interpolation, float foamSharpness, int seed, float x, float y, float z, float w, float u) {
+    public static final float singleFoam(int interpolation, float foamSharpness, boolean _normalizeNoise, int seed, float x, float y, float z, float w, float u) {
         final float p0 = x *  0.8157559148337911f + y *  0.5797766823136037f;
         final float p1 = x * -0.7314923478726791f + y *  0.6832997137249108f;
         final float p2 = x * -0.0208603044412437f + y * -0.3155296974329846f + z * 0.9486832980505138f;
@@ -130,7 +140,8 @@ public class FnFoam extends FastNoiseLite
         float zin = p3;
         float win = p4;
         float uin = p5;
-        final float a = singleValue(interpolation, seed, xin, yin, zin, win, uin);
+        float a = singleValue(interpolation, seed, xin, yin, zin, win, uin);
+        if(_normalizeNoise) a = (a * .5f) + .5f;
         seed += 0x9E3779BD;
         seed ^= seed >>> 14;
         xin = p0;
@@ -138,7 +149,8 @@ public class FnFoam extends FastNoiseLite
         zin = p3;
         win = p4;
         uin = p5;
-        final float b = singleValue(interpolation, seed, xin + a, yin, zin, win, uin);
+        float b = singleValue(interpolation, seed, xin + a, yin, zin, win, uin);
+        if(_normalizeNoise) b = (b * .5f) + .5f;
         seed += 0x9E3779BD;
         seed ^= seed >>> 14;
         xin = p0;
@@ -146,7 +158,8 @@ public class FnFoam extends FastNoiseLite
         zin = p3;
         win = p4;
         uin = p5;
-        final float c = singleValue(interpolation, seed, xin + b, yin, zin, win, uin);
+        float c = singleValue(interpolation, seed, xin + b, yin, zin, win, uin);
+        if(_normalizeNoise) c = (c * .5f) + .5f;
         seed += 0x9E3779BD;
         seed ^= seed >>> 14;
         xin = p0;
@@ -154,7 +167,8 @@ public class FnFoam extends FastNoiseLite
         zin = p2;
         win = p4;
         uin = p5;
-        final float d = singleValue(interpolation, seed, xin + c, yin, zin, win, uin);
+        float d = singleValue(interpolation, seed, xin + c, yin, zin, win, uin);
+        if(_normalizeNoise) d = (d * .5f) + .5f;
         seed += 0x9E3779BD;
         seed ^= seed >>> 14;
         xin = p0;
@@ -162,7 +176,8 @@ public class FnFoam extends FastNoiseLite
         zin = p2;
         win = p3;
         uin = p5;
-        final float e = singleValue(interpolation, seed, xin + d, yin, zin, win, uin);
+        float e = singleValue(interpolation, seed, xin + d, yin, zin, win, uin);
+        if(_normalizeNoise) e = (e * .5f) + .5f;
         seed += 0x9E3779BD;
         seed ^= seed >>> 14;
         xin = p0;
@@ -170,8 +185,8 @@ public class FnFoam extends FastNoiseLite
         zin = p2;
         win = p3;
         uin = p4;
-        final float f = singleValue(interpolation, seed, xin + e, yin, zin, win, uin);
-
+        float f = singleValue(interpolation, seed, xin + e, yin, zin, win, uin);
+        if(_normalizeNoise) f = (f * .5f) + .5f;
         final float result = (a + b + c + d + e + f) * 0.16666666666666666f;
         final float sharp = foamSharpness * 5.5f;
         final float diff = 0.5f - result;
@@ -179,7 +194,7 @@ public class FnFoam extends FastNoiseLite
         return (((result + sign)) / (Float.MIN_VALUE - sign + (result + sharp * diff) * one) - sign - sign) - 1f;
     }
 
-    public static final float singleFoam(int interpolation, float foamSharpness, int seed, float x, float y, float z, float w, float u, float v) {
+    public static final float singleFoam(int interpolation, float foamSharpness, boolean _normalizeNoise, int seed, float x, float y, float z, float w, float u, float v) {
         final float p0 = x;
         final float p1 = x * -0.16666666666666666f + y *  0.98601329718326940f;
         final float p2 = x * -0.16666666666666666f + y * -0.19720265943665383f + z *  0.96609178307929590f;
@@ -193,7 +208,8 @@ public class FnFoam extends FastNoiseLite
         float win = p6;
         float uin = p1;
         float vin = p4;
-        final float a = singleValue(interpolation, seed, xin, yin, zin, win, uin, vin);
+        float a = singleValue(interpolation, seed, xin, yin, zin, win, uin, vin);
+        if(_normalizeNoise) a = (a * .5f) + .5f;
         seed += 0x9E3779BD;
         seed ^= seed >>> 14;
         xin = p2;
@@ -202,7 +218,8 @@ public class FnFoam extends FastNoiseLite
         win = p4;
         uin = p5;
         vin = p3;
-        final float b = singleValue(interpolation, seed, xin + a, yin, zin, win, uin, vin);
+        float b = singleValue(interpolation, seed, xin + a, yin, zin, win, uin, vin);
+        if(_normalizeNoise) b = (b * .5f) + .5f;
         seed += 0x9E3779BD;
         seed ^= seed >>> 14;
         xin = p1;
@@ -211,7 +228,8 @@ public class FnFoam extends FastNoiseLite
         win = p4;
         uin = p6;
         vin = p5;
-        final float c = singleValue(interpolation, seed, xin + b, yin, zin, win, uin, vin);
+        float c = singleValue(interpolation, seed, xin + b, yin, zin, win, uin, vin);
+        if(_normalizeNoise) c = (c * .5f) + .5f;
         seed += 0x9E3779BD;
         seed ^= seed >>> 14;
         xin = p6;
@@ -220,7 +238,8 @@ public class FnFoam extends FastNoiseLite
         win = p5;
         uin = p4;
         vin = p1;
-        final float d = singleValue(interpolation, seed, xin + c, yin, zin, win, uin, vin);
+        float d = singleValue(interpolation, seed, xin + c, yin, zin, win, uin, vin);
+        if(_normalizeNoise) d = (d * .5f) + .5f;
         seed += 0x9E3779BD;
         seed ^= seed >>> 14;
         xin = p2;
@@ -229,7 +248,8 @@ public class FnFoam extends FastNoiseLite
         win = p0;
         uin = p3;
         vin = p6;
-        final float e = singleValue(interpolation, seed, xin + d, yin, zin, win, uin, vin);
+        float e = singleValue(interpolation, seed, xin + d, yin, zin, win, uin, vin);
+        if(_normalizeNoise) e = (e * .5f) + .5f;
         seed += 0x9E3779BD;
         seed ^= seed >>> 14;
         xin = p0;
@@ -238,7 +258,8 @@ public class FnFoam extends FastNoiseLite
         win = p3;
         uin = p1;
         vin = p2;
-        final float f = singleValue(interpolation, seed, xin + e, yin, zin, win, uin, vin);
+        float f = singleValue(interpolation, seed, xin + e, yin, zin, win, uin, vin);
+        if(_normalizeNoise) f = (f * .5f) + .5f;
         seed += 0x9E3779BD;
         seed ^= seed >>> 14;
         xin = p5;
@@ -247,7 +268,8 @@ public class FnFoam extends FastNoiseLite
         win = p3;
         uin = p4;
         vin = p0;
-        final float g = singleValue(interpolation, seed, xin + f, yin, zin, win, uin, vin);
+        float g = singleValue(interpolation, seed, xin + f, yin, zin, win, uin, vin);
+        if(_normalizeNoise) g = (g * .5f) + .5f;
         final float result = (a + b + c + d + e + f + g) * 0.14285714285714285f;
         final float sharp = foamSharpness * 6.6f;
         final float diff = 0.5f - result;
@@ -255,7 +277,7 @@ public class FnFoam extends FastNoiseLite
         return (((result + sign)) / (Float.MIN_VALUE - sign + (result + sharp * diff) * one) - sign - sign) - 1f;
     }
 
-    public static final float singleFoam(int interpolation, float foamSharpness, int seed, float x, float y, float z, float w, float u, float v, float m) {
+    public static final float singleFoam(int interpolation, float foamSharpness, boolean _normalizeNoise, int seed, float x, float y, float z, float w, float u, float v, float m) {
         final float p0 = x;
         final float p1 = x * -0.14285714285714285f + y * +0.9897433186107870f;
         final float p2 = x * -0.14285714285714285f + y * -0.1649572197684645f + z * +0.97590007294853320f;
@@ -271,7 +293,8 @@ public class FnFoam extends FastNoiseLite
         float uin = p1;
         float vin = p4;
         float min = p5;
-        final float a = singleValue(interpolation, seed, xin, yin, zin, win, uin, vin, min);
+        float a = singleValue(interpolation, seed, xin, yin, zin, win, uin, vin, min);
+        if(_normalizeNoise) a = (a * .5f) + .5f;
         seed += 0x9E377;
         xin = p2;
         yin = p3;
@@ -280,7 +303,8 @@ public class FnFoam extends FastNoiseLite
         uin = p6;
         vin = p5;
         min = p7;
-        final float b = singleValue(interpolation, seed, xin + a, yin, zin, win, uin, vin, min);
+        float b = singleValue(interpolation, seed, xin + a, yin, zin, win, uin, vin, min);
+        if(_normalizeNoise) b = (b * .5f) + .5f;
         seed += 0x9E377;
         xin = p1;
         yin = p2;
@@ -289,7 +313,8 @@ public class FnFoam extends FastNoiseLite
         uin = p5;
         vin = p7;
         min = p6;
-        final float c = singleValue(interpolation, seed, xin + b, yin, zin, win, uin, vin, min);
+        float c = singleValue(interpolation, seed, xin + b, yin, zin, win, uin, vin, min);
+        if(_normalizeNoise) c = (c * .5f) + .5f;
         seed += 0x9E377;
         xin = p7;
         yin = p0;
@@ -298,7 +323,8 @@ public class FnFoam extends FastNoiseLite
         uin = p4;
         vin = p6;
         min = p1;
-        final float d = singleValue(interpolation, seed, xin + c, yin, zin, win, uin, vin, min);
+        float d = singleValue(interpolation, seed, xin + c, yin, zin, win, uin, vin, min);
+        if(_normalizeNoise) d = (d * .5f) + .5f;
         seed += 0x9E377;
         xin = p3;
         yin = p1;
@@ -307,7 +333,8 @@ public class FnFoam extends FastNoiseLite
         uin = p7;
         vin = p0;
         min = p2;
-        final float e = singleValue(interpolation, seed, xin + d, yin, zin, win, uin, vin, min);
+        float e = singleValue(interpolation, seed, xin + d, yin, zin, win, uin, vin, min);
+        if(_normalizeNoise) e = (e * .5f) + .5f;
         seed += 0x9E377;
         xin = p4;
         yin = p7;
@@ -316,7 +343,8 @@ public class FnFoam extends FastNoiseLite
         uin = p0;
         vin = p1;
         min = p3;
-        final float f = singleValue(interpolation, seed, xin + e, yin, zin, win, uin, vin, min);
+        float f = singleValue(interpolation, seed, xin + e, yin, zin, win, uin, vin, min);
+        if(_normalizeNoise) f = (f * .5f) + .5f;
         seed += 0x9E377;
         xin = p5;
         yin = p4;
@@ -325,7 +353,8 @@ public class FnFoam extends FastNoiseLite
         uin = p2;
         vin = p3;
         min = p0;
-        final float g = singleValue(interpolation, seed, xin + f, yin, zin, win, uin, vin, min);
+        float g = singleValue(interpolation, seed, xin + f, yin, zin, win, uin, vin, min);
+        if(_normalizeNoise) g = (g * .5f) + .5f;
         seed += 0x9E377;
         xin = p6;
         yin = p5;
@@ -334,7 +363,8 @@ public class FnFoam extends FastNoiseLite
         uin = p3;
         vin = p2;
         min = p4;
-        final float h = singleValue(interpolation, seed, xin + g, yin, zin, win, uin, vin, min);
+        float h = singleValue(interpolation, seed, xin + g, yin, zin, win, uin, vin, min);
+        if(_normalizeNoise) h = (h * .5f) + .5f;
         final float result = (a + b + c + d + e + f + g + h) * 0.125f;
         final float sharp = foamSharpness * 7.7f;
         final float diff = 0.5f - result;
