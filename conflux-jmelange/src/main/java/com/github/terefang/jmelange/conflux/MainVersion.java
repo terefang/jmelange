cf950b8dc46541fc0dfb5b3ab2f7e2ba3a6f2d35
BuildTools <unconfigured@null.spigotmc.org>
2022-11-23 23:02:25 +0100

    2022.11.208

--- conflux-jmelange/src/main/java/com/github/terefang/jmelange/conflux/MainVersion.java
+++ conflux-jmelange/src/main/java/com/github/terefang/jmelange/conflux/MainVersion.java
@@ -2,5 +2,5 @@ package com.github.terefang.jmelange.conflux;
 
 public class MainVersion
 {
-    public static final String VERSION = "2022.11.207";
+    public static final String VERSION = "2022.11.208";
 }
--- pdf-jmelange/src/main/java/com/github/terefang/jmelange/pdf/MainVersion.java
+++ pdf-jmelange/src/main/java/com/github/terefang/jmelange/pdf/MainVersion.java
@@ -2,5 +2,5 @@ package com.github.terefang.pdfgen;
 
 public class MainVersion
 {
-    public static final String VERSION = "2022.11.207-2022.11.23.1152";
+    public static final String VERSION = "2022.11.208-2022.11.23.2201";
 }
